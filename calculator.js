0fef3b89bc13540a1fefea884ea28f6c8ac3a825
alexisko <alexisko@hawaii.edu>
2017-04-28 10:55:16 -1000

    first two functions added

--- calculator.js
+++ calculator.js
@@ -5,19 +5,39 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+function calculatorModule() {
+  var memory, total = 0;
+  var calculator = {
+
+    /**
+     * sets the `total` to the number passed in
+     * @param  { Number } x
+     * @return { Number }    current total
+     */
+    load: function(num) {
+      if(typeof num === "number") {
+        total = num;
+        return total;
+      }
+      return "Error: thats not a number, try again!\n";
+    },
+
+    /**
+     * Return the value of `total`
+     * @return { Number }
+     */
+    getTotal: function() {
+      return total;
+    }
+  };
+
+  return calculator;
+}
+
+  
 
 
-  /**
-   * sets the `total` to the number passed in
-   * @param  { Number } x
-   * @return { Number }    current total
-   */
-
-
-  /**
-   * Return the value of `total`
-   * @return { Number }
-   */
+  
 
 
   /**
