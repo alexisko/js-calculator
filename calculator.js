9cdfc190f7e6c8de279f9fd297c5d30fae67bc1c
Alexis <alexisko@hawaii.edu>
2017-05-17 21:16:21 -1000

    Fixed multiply throw error

--- calculator.js
+++ calculator.js
@@ -63,7 +63,7 @@ function calculatorModule() {
         total *= num;
         return total;
       }
-      return "Error: thats not a number, try again!\n";
+      throw new Error("thats not a number, try again!\n");
     },
 
     /**
